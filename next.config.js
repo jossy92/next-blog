b0e2af7458de036b05dc3828215392ed820e2917
joe <jossyojih@gmail.com>
2021-01-27 09:38:57 +0100

    config hidden

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        JWT :'abcdefg',
-        MONGO_URI: 'mongodb://localhost/nextblog',
-        ATLAS_URI: 'mongodb+srv://joseph:okpanachi@cluster0.ghsi1.gcp.mongodb.net/NEXT-BLOG'
-      
-    }
-}
